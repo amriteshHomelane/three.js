9cbc7e4e3b6949bc3d9fb95cace071a7d639ea1f
Satbir Singh <satbir.s@homelane.com>
2017-08-23 19:46:35 +0530

    Fixing RectAreaLight Helper.

--- src/extras/helpers/RectAreaLightHelper.js
+++ src/extras/helpers/RectAreaLightHelper.js
@@ -7,6 +7,7 @@ import { Object3D } from '../../core/Object3D';
 import { Vector3 } from '../../math/Vector3';
 import { Mesh } from '../../objects/Mesh';
 import { MeshBasicMaterial } from '../../materials/MeshBasicMaterial';
+import { MeshStandardMaterial } from '../../materials/MeshStandardMaterial';
 import { BufferGeometry } from '../../core/BufferGeometry';
 import { BufferAttribute } from '../../core/BufferAttribute';
 
@@ -17,7 +18,7 @@ function RectAreaLightHelper( light ) {
 	this.light = light;
 	this.light.updateMatrixWorld();
 
-	var materialFront = new MeshBasicMaterial( {
+	this.materialFront = new MeshStandardMaterial( {
 		color: light.color,
 		fog: false
 	} );
@@ -31,14 +32,17 @@ function RectAreaLightHelper( light ) {
 	var geometry = new BufferGeometry();
 
 	geometry.addAttribute( 'position', new BufferAttribute( new Float32Array( 6 * 3 ), 3 ) );
+	geometry.addAttribute( 'uv', new BufferAttribute( new Float32Array( 6 * 2 ), 2 ) );
+
+	var lightShape = new THREE.PlaneGeometry( this.light.width, this.light.height );
 
 	// shows the "front" of the light, e.g. where light comes from
 
-	this.add( new Mesh( geometry, materialFront ) );
+	this.add( new Mesh( geometry, this.materialFront ) );
 
 	// shows the "back" of the light, which does not emit light
 
-	this.add( new Mesh( geometry, materialBack ) );
+	//this.add( new Mesh( geometry, materialBack ) );
 
 	this.update();
 
@@ -51,8 +55,8 @@ RectAreaLightHelper.prototype.dispose = function () {
 
 	this.children[ 0 ].geometry.dispose();
 	this.children[ 0 ].material.dispose();
-	this.children[ 1 ].geometry.dispose();
-	this.children[ 1 ].material.dispose();
+	//this.children[ 1 ].geometry.dispose();
+	//this.children[ 1 ].material.dispose();
 
 };
 
@@ -64,7 +68,7 @@ RectAreaLightHelper.prototype.update = function () {
 	return function update() {
 
 		var mesh1 = this.children[ 0 ];
-		var mesh2 = this.children[ 1 ];
+		// var mesh2 = this.children[ 1 ];
 
 		if ( this.light.target ) {
 
@@ -73,14 +77,16 @@ RectAreaLightHelper.prototype.update = function () {
 
 			var lookVec = vector2.clone().sub( vector1 );
 			mesh1.lookAt( lookVec );
-			mesh2.lookAt( lookVec );
+			// mesh2.lookAt( lookVec );
 
 		}
 
 		// update materials
 
-		mesh1.material.color.copy( this.light.color ).multiplyScalar( this.light.intensity );
-		mesh2.material.color.copy( this.light.color ).multiplyScalar( this.light.intensity );
+			var size = this.light.width * this.light.height/4;
+			mesh1.material.color.copy( this.light.color ).multiplyScalar( this.light.intensity/size );
+		// mesh1.material.color.copy( this.light.color ).multiplyScalar( this.light.intensity );
+		// mesh2.material.color.copy( this.light.color ).multiplyScalar( this.light.intensity );
 
 		// calculate new dimensions of the helper
 
@@ -90,6 +96,8 @@ RectAreaLightHelper.prototype.update = function () {
 		// because the buffer attribute is shared over both geometries, we only have to update once
 
 		var position = mesh1.geometry.getAttribute( 'position' );
+		var uvs = mesh1.geometry.getAttribute( 'uv' );
+		var uvArray = uvs.array;
 		var array = position.array;
 
 		// first face
@@ -97,6 +105,10 @@ RectAreaLightHelper.prototype.update = function () {
 		array[  0 ] =   hx; array[  1 ] = - hy; array[  2 ] = 0;
 		array[  3 ] =   hx; array[  4 ] =   hy; array[  5 ] = 0;
 		array[  6 ] = - hx; array[  7 ] =   hy; array[  8 ] = 0;
+		
+		uvArray[  0 ] =   1; uvArray[  1 ] =   0;
+		uvArray[  2 ] =   1; uvArray[  3 ] =   1;
+		uvArray[  4 ] =   0; uvArray[  5 ] =   1;
 
 		// second face
 
@@ -104,8 +116,12 @@ RectAreaLightHelper.prototype.update = function () {
 		array[ 12 ] = - hx; array[ 13 ] = - hy; array[ 14 ] = 0;
 		array[ 15 ] =   hx; array[ 16 ] = - hy; array[ 17 ] = 0;
 
-		position.needsUpdate = true;
+		uvArray[  6 ] = 0; uvArray[ 7 ] =   1;
+		uvArray[ 8 ] =  0; uvArray[ 9 ] = 	0;
+		uvArray[ 10 ] = 1; uvArray[ 11 ] =   0;
 
+		position.needsUpdate = true;
+		uvs.needsUpdate = true;
 	};
 
 }();
