d2fd0c223f1b4f253a0fc64dfc32335a5b8220f2
Satbir Singh <satbir.s@homelane.com>
2017-12-30 19:27:16 +0530

    Publish to s3.

---
+++ publish.js
@@ -0,0 +1,40 @@
+var version = require('./releaseConfig').version;
+
+var fileName = 'three-' + version + '.min.js';
+
+var s3 = require('s3');
+
+var client = s3.createClient({
+  maxAsyncS3: 20,     // this is the default
+  s3RetryCount: 3,    // this is the default
+  s3RetryDelay: 1000, // this is the default
+  multipartUploadThreshold: 20971520, // this is the default (20 MB)
+  multipartUploadSize: 15728640, // this is the default (15 MB)
+  s3Options: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+  }
+});
+
+var params = {
+  localFile: "./build/three.min.js",
+  s3Params: {
+    Bucket: "spacecraft-dev",
+    Key: "threejs/" + fileName,
+    ACL: 'public-read'
+    // other options supported by putObject, except Body and ContentLength.
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+  },
+};
+var uploader = client.uploadFile(params);
+uploader.on('error', function(err) {
+  console.error("unable to upload:", err.stack);
+});
+uploader.on('progress', function() {
+  console.log("progress", uploader.progressMd5Amount,
+            uploader.progressAmount, uploader.progressTotal);
+});
+uploader.on('end', function() {
+  console.log("done uploading");
+});
